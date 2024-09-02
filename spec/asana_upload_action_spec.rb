a207fdee88bd26b877c973dfbf45038eff19ec17
Jacek Łyp <jaceklyp@gmail.com>
2024-09-02 12:07:33 +0200

    Add AsanaUploadAction (#2)
    Task URL: https://app.asana.com/0/1201392122292466/1208137627434497/f
    This change adds new lane that replaces asana-upload action

---
+++ spec/asana_upload_action_spec.rb
@@ -0,0 +1,35 @@
+describe Fastlane::Actions::AsanaUploadAction do
+  describe "#run" do
+    it "uploads a file successfully" do
+      allow(HTTParty).to receive(:post).and_return(double(success?: true))
+      allow(File).to receive(:open).with("path/to/file.txt").and_return(double)
+
+      expect { test_action("12345", "path/to/file.txt") }.not_to raise_error
+    end
+
+    it "shows error if HTTP failure" do
+      allow(HTTParty).to receive(:post).and_return(
+        double(
+          success?: false,
+          code: 500,
+          message: "Internal Server Error"
+        )
+      )
+      allow(File).to receive(:open).with("path/to/file.txt").and_return(double)
+
+      expect(Fastlane::UI).to receive(:user_error!).with("Failed to upload file to Asana task: (500 Internal Server Error)")
+      test_action("12345", "path/to/file.txt")
+    end
+
+    it "shows error if file does not exist" do
+      expect(Fastlane::UI).to receive(:user_error!).with("Failed to open file: path/to/file.txt")
+      expect(HTTParty).not_to receive(:post)
+      test_action("12345", "path/to/file.txt")
+    end
+  end
+
+  def test_action(task_id, file_name)
+    Fastlane::Actions::AsanaUploadAction.run(task_id: task_id,
+                                             file_name: file_name)
+  end
+end
