547bbcc25ba7f5b87eff21ca067332b3b47b7231
Jacek Łyp <jlyp@duckduckgo.com>
2024-09-02 13:26:56 +0200

    Fix naming

---
+++ lib/fastlane/plugin/ddg_apple_automation/actions/asana_get_user_id_for_github_handle_action.rb
@@ -0,0 +1,58 @@
+require "fastlane/action"
+require "fastlane_core/configuration/config_item"
+require "yaml"
+require_relative "../helper/ddg_apple_automation_helper"
+require_relative "../helper/github_actions_helper"
+
+module Fastlane
+  module Actions
+    class AsanaGetUserIdForGithubHandleAction < Action
+      def self.run(params)
+        token = params[:asana_access_token]
+        github_handle = params[:github_handle]
+
+        mapping_file = File.expand_path('assets/github-asana-user-id-mapping.yml', __dir__)
+        user_mapping = YAML.load_file(mapping_file)
+        asana_user_id = user_mapping[github_handle]
+
+        if asana_user_id.nil? || asana_user_id.to_s.empty?
+          UI.user_error!("Asana User ID not found for GitHub handle: #{github_handle}")
+        else
+          Helper::GitHubActionsHelper.set_output("user-id", asana_user_id)
+          asana_user_id
+        end
+      end
+
+      def self.description
+        "This action returns Asana user ID that matches GitHub user handle"
+      end
+
+      def self.authors
+        ["DuckDuckGo"]
+      end
+
+      def self.return_value
+        "User ID that matches GitHub user handle"
+      end
+
+      def self.details
+        # Optional:
+        ""
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.asana_access_token,
+          FastlaneCore::ConfigItem.new(key: :github_handle,
+                                       description: "Github user handle",
+                                       optional: false,
+                                       type: String)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
