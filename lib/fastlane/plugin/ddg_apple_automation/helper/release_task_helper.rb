98070ca67fa9abe7eaf1a8bee7270a21559dd0b3
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-10-01 17:36:37 -0400

    Sanitize generated Asana task description

--- lib/fastlane/plugin/ddg_apple_automation/helper/release_task_helper.rb
+++ lib/fastlane/plugin/ddg_apple_automation/helper/release_task_helper.rb
@@ -1,4 +1,5 @@
 require "fastlane_core/ui/ui"
+require_relative "asana_helper"
 require_relative "ddg_apple_automation_helper"
 require_relative "github_actions_helper"
 require_relative "release_notes/asana_release_notes_extractor"
@@ -10,10 +11,11 @@ module Fastlane
     class ReleaseTaskHelper
       def self.construct_release_task_description(release_notes, task_ids)
         template_file = Helper::DdgAppleAutomationHelper.path_for_asset_file("release_task_helper/templates/release_task_description.html.erb")
-        Helper::DdgAppleAutomationHelper.process_erb_template(template_file, {
+        html_notes = Helper::DdgAppleAutomationHelper.process_erb_template(template_file, {
           release_notes: release_notes,
           task_ids: task_ids
         })
+        Helper::AsanaHelper.sanitize_asana_html_notes(html_notes)
       end
 
       def self.extract_release_notes(task_body, output_type: "html")
