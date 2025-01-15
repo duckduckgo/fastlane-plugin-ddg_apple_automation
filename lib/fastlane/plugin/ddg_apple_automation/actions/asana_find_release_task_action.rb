27027b6e02aa514c95f5045bedeb9971a7ee68a8
Kieran <kshannon@duckduckgo.com>
2025-01-15 22:34:29 +1100

    add test value for branch

--- lib/fastlane/plugin/ddg_apple_automation/actions/asana_find_release_task_action.rb
+++ lib/fastlane/plugin/ddg_apple_automation/actions/asana_find_release_task_action.rb
@@ -53,6 +53,11 @@ module Fastlane
 
         release_task_url = Helper::AsanaHelper.asana_task_url(release_task_id)
         release_branch = "release/#{latest_marketing_version}"
+        
+        if ENV['TEST_MODE']
+          release_branch = "test-release/#{latest_marketing_version}"
+        end
+
         UI.success("Found #{latest_marketing_version} release task: #{release_task_url}")
 
         Helper::GitHubActionsHelper.set_output("release_branch", release_branch)
