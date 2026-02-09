e811543daa7e899bcda0e3784a4544ff10a5d0df
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-02-09 16:08:21 +0100

    Fix rubocop complaint

--- lib/fastlane/plugin/ddg_apple_automation/helper/asana_helper.rb
+++ lib/fastlane/plugin/ddg_apple_automation/helper/asana_helper.rb
@@ -426,7 +426,13 @@ module Fastlane
           .gsub("\n", " ")
           .scan(%r{\b\*{2}?Task/Issue URL:\*{2}?\s*https://app\.asana\.com[/0-9a-z]+\b})
           .map { |task_line| task_line.gsub(/.*(https.*)/, '\1') }
-          .filter_map { |task_url| extract_asana_task_id(task_url, set_gha_output: false) rescue nil }
+          .filter_map do |task_url|
+            begin
+              extract_asana_task_id(task_url, set_gha_output: false)
+            rescue StandardError
+              nil
+            end
+          end
           .uniq
       end
 
